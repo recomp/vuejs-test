d93c2b864f639f7169794581b9acd940493cbdd9
Pavel Bakin <pavel@bakin.pro>
2021-12-10 23:34:46 +0300

    clean repo

--- src/filters/currency.js
+++ src/filters/currency.js
@@ -1,5 +1,4 @@
 export const moneyFormat = function formatMoney(string) {
   const price = Number.prototype.toFixed.call(parseFloat(string) || 0, 2);
-  // console.log('moneyFormat', price.match('.'));
   return price.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ');
 };
